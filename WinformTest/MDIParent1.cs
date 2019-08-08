44f65d25dbc64f0b03aa116f94d115a70cc51043
Compunet IT <Compunet IT@DESKTOP-CKDGJ8C>
2019-08-08 16:13:18 +0100

    ok

--- WinformTest/MDIParent1.cs
+++ WinformTest/MDIParent1.cs
@@ -114,7 +114,7 @@ namespace WinformTest
         {
             string fp = "Viva kenstars";
             String st = textBox1.Text;
-            MessageBox.Show("Hello" + " " + st + "Welcome");
+            MessageBox.Show("Hello" + " " + st + "  Welcome");
         }
     }
 }
